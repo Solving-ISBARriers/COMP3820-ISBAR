5c06ce6de202457a31a63ecd85ecee6448780994
JamaJ1701 <48624318+JamaJ1701@users.noreply.github.com>
2021-10-19 11:12:54 +1000

    yeah

--- isbar-complex-react/src/app-complex/Introduction.js
+++ isbar-complex-react/src/app-complex/Introduction.js
@@ -7,7 +7,7 @@ export class Introduction extends React.Component {
     }
     render(){
         return(
-            <div>Introduction</div>
+            <div>Introduction is changed</div>
         )
     }
 }
