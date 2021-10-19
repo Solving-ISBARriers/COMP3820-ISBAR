f627336bd904e358caaed7015ec0bef105acf1b5
JamaJ1701 <48624318+JamaJ1701@users.noreply.github.com>
2021-10-19 11:27:35 +1000

    css taken out a bit

--- isbar-complex-react/src/app-complex/Assessment.js
+++ isbar-complex-react/src/app-complex/Assessment.js
@@ -8,105 +8,105 @@ export class Assessment extends React.Component {
     render(){
         return(
             <div>
-                <div class="title">
+                <div className="title">
         <p>Assessment</p>
     </div>
 
-    <div class="main">
-        <div class="content">
-            <textarea class="textarea" rows="6">Latest clinical assessment :</textarea>
-            <div class="sign">
-                <p class="sign-title">Vital signs time</p>
+    <div className="main">
+        <div className="content">
+            <textarea className="textarea" rows="6" value="Latest clinical assessment :" />
+            <div className="sign">
+                <p className="sign-title">Vital signs time</p>
                 <ul>
                     <li>
-                        <div style="width:100%;">
-                            <div class="time" style="float: left;position: relative;display:flex;flex-wrap: inherit;">
-                                <p class="p1">AM</p>
-                                <p class="p2">PM</p>
+                        <div>
+                            <div className="time" >
+                                <p className="p1">AM</p>
+                                <p className="p2">PM</p>
                             </div>
-                            <input style="width: 8%;height:35px;margin-left: 1%;font-size: 18px" />
+                            <input />
                             :
-                            <input style="width: 8%;height:35px;font-size: 18px" />
+                            <input />
 
-                            <input style="width: 10%;height:35px;margin-left: 3%;font-size: 18px" placeholder="DD" />
+                            <input />
                             /
-                            <input style="width: 10%;height:35px;margin-left: 3%;font-size: 18px" placeholder="MM" />
+                            <input />
                             /
-                            <input style="width: 12%;height:35px;margin-left: 3%;font-size: 18px" placeholder="YYYY" />
+                            <input />
                         </div>
                     </li>
                     <li>
                         <label>Temp.</label>
-                        <input type="text" class="input" />
+                        <input type="text" className="input" />
                     </li>
                     <li>
                         <label>Resp rate</label>
-                        <input type="text" class="input" />
+                        <input type="text" className="input" />
                     </li>
                     <li>
                         <label>Pulse</label>
-                        <input type="text" class="input" />
+                        <input type="text" className="input" />
                     </li>
                     <li>
                         <label>Pain Score</label>
-                        <input type="text" class="input" />
+                        <input type="text" className="input" />
                     </li>
                     <li>
                         <label>SpO2</label>
-                        <input type="text" class="input" />
+                        <input type="text" className="input" />
                     </li>
                     <li>
                         <label>O2 rate/device</label>
-                        <input type="text" class="input" />
+                        <input type="text" className="input" />
                     </li>
                     <li>
                         <label>B.P</label>
-                        <input type="text" class="input" />
+                        <input type="text" className="input" />
                     </li>
                     <li>
                         <label>Urine output</label>
-                        <input type="text" class="input" />
+                        <input type="text" className="input" />
                     </li>
                     <li>
                         <label>Hb</label>
-                        <input type="text" class="input" />
+                        <input type="text" className="input" />
                     </li>
                 </ul>
             </div>
         </div>
     </div>
 
-    <div class="foot">
-        <ul class="foot-ul">
-            <li class="foot-li" id="arrow-left">
+    <div className="foot">
+        <ul className="foot-ul">
+            <li className="foot-li" id="arrow-left">
                 <img src="img/arrow1.png" width="60" />
             </li>
-            <li class="foot-li">
-                <div class="foot-li-div foot-li-div-0">
+            <li className="foot-li">
+                <div className="foot-li-div foot-li-div-0">
                     <p>I</p>
                 </div>
             </li>
-            <li class="foot-li">
-                <div class="foot-li-div foot-li-div-1">
+            <li className="foot-li">
+                <div className="foot-li-div foot-li-div-1">
                     <p>S</p>
                 </div>
             </li>
-            <li class="foot-li">
-                <div class="foot-li-div foot-li-div-2">
+            <li className="foot-li">
+                <div className="foot-li-div foot-li-div-2">
                     <p>B</p>
                 </div>
             </li>
-            <li class="foot-li">
-                <div class="foot-li-div foot-li-div-3 checked">
+            <li className="foot-li">
+                <div className="foot-li-div foot-li-div-3 checked">
                     <p>A</p>
                 </div>
             </li>
-            <li class="foot-li">
-                <div class="foot-li-div foot-li-div-4">
+            <li className="foot-li">
+                <div className="foot-li-div foot-li-div-4">
                     <p>R</p>
                 </div>
             </li>
-            <li class="foot-li" id="arrow-right">
+            <li className="foot-li" id="arrow-right">
                 <img src="img/arrow2.png" width="60" />
             </li>
         </ul>
