a063c0c6c55b64b9ed9edacce8fde8e14bcc542a
oscardunstan <oscardunstan2@gmail.com>
2021-10-28 09:51:47 +1000

    Added Extra table fields

--- isbar-complex-react/src/app-complex/Situation.js
+++ isbar-complex-react/src/app-complex/Situation.js
@@ -55,29 +55,61 @@ export class Situation extends React.Component {
                 <Patient />
                 <mb-fhir-form class="flex flex-col gap-6 m-5 " onClick={this.handleSubmit} >               
                     <mb-context path="resourceType" data="Patient"> </mb-context>
-                    <mb-date label="Date of transfer" /> 
+                    <mb-date label="Date of transfer"> </mb-date>
                     <mb-input  path="name[0].given" label="Patient Status" class=" "> </mb-input>
                     <sl-textarea size="medium" resize="auto" label="Principal diagnosis/problem"></sl-textarea>
                     <sl-textarea size="medium" resize="auto" label="Other diagnosis/problem"></sl-textarea>
                     <mb-input path="name[0].given" label="Reason for transfer"></mb-input>
                     <div class="table-container">
+                        <h3>Observations</h3>
                         <table>
                             <tr>
                                 <th>Airway</th>
                                 <th>Breathing</th>
+                                <th>Skin Colour</th>
+                                <th>Mental</th>
+                                <th>Behaviour</th>
                             </tr>
                             <tr>
                                 <td><sl-checkbox>Patent</sl-checkbox></td>
                                 <td><sl-checkbox>Unremarkable</sl-checkbox></td>
+                                <td><sl-checkbox>Unremarkable</sl-checkbox></td>
+                                <td><sl-checkbox>Cognitive impairment</sl-checkbox></td>
+                                <td><sl-checkbox>Verbal aggression</sl-checkbox></td>
                             </tr>
                             <tr>
                                 <td><sl-checkbox>Compromised</sl-checkbox></td>
                                 <td><sl-checkbox>Shallow</sl-checkbox></td>
+                                <td><sl-checkbox>Pale</sl-checkbox></td>
+                                <td><sl-checkbox>Depression</sl-checkbox></td>
+                                <td><sl-checkbox>Harm to others</sl-checkbox></td>
                             </tr>
                             <tr>
                                 <td><sl-checkbox>Ventilated</sl-checkbox></td>
                                 <td><sl-checkbox>Deep</sl-checkbox></td>
-                            </tr>                           
+                                <td><sl-checkbox>Flushed</sl-checkbox></td>
+                                <td><sl-checkbox>Dementia</sl-checkbox></td>
+                                <td><sl-checkbox>Harm to self</sl-checkbox></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td><sl-checkbox>Rapid</sl-checkbox></td>
+                                <td><sl-checkbox>Mottled</sl-checkbox></td>
+                                <td><sl-checkbox>Delirium</sl-checkbox></td>
+                                <td><sl-checkbox>Sleep disturbance</sl-checkbox></td>
+                            </tr> 
+                            <tr>
+                                <td></td>
+                                <td><sl-checkbox>Slow</sl-checkbox></td>
+                                <td><sl-checkbox>Cyanotic</sl-checkbox></td>
+                                <td><sl-checkbox>Delirium</sl-checkbox></td>
+                                <td><sl-checkbox>Sleep disturbance</sl-checkbox></td>
+                            </tr> 
+                            <tr>
+                                <td></td>
+                                <td><sl-checkbox>Audible Wheeze</sl-checkbox></td>
+                                <td></td>
+                            </tr>                         
                             
                         </table>
                     </div>
