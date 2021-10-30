214b9e2aa9cada92169bc1292d7849cce34236e6
oscardunstan <oscardunstan2@gmail.com>
2021-10-30 21:16:55 +1000

    Squashed commit of the following:
    commit 3ef47b4519b0ec46d0735c33fcbaf14d7c7b9701
    Author: JamaJ1701 <48624318+JamaJ1701@users.noreply.github.com>
    Date:   Sat Oct 30 10:09:24 2021 +1000
    
        --
    
    commit b1491fba2af71c06db60b460d090b8308f535775
    Author: JamaJ1701 <48624318+JamaJ1701@users.noreply.github.com>
    Date:   Sat Oct 30 10:07:50 2021 +1000
    
        gitignre edit pushed:
        :
    
    commit 0a2d8586c77ff5fa026eb5864c11475c23760557
    Author: JamaJ1701 <48624318+JamaJ1701@users.noreply.github.com>
    Date:   Sat Oct 30 10:07:22 2021 +1000
    
        gitignore edited
    
    commit 15231090042598f9070c405aa1710273d3239b66
    Author: JamaJ1701 <48624318+JamaJ1701@users.noreply.github.com>
    Date:   Fri Oct 29 23:40:24 2021 +1000
    
        History near completion, author and reci[ient information displayed
    
    commit b463dc51209dc3b8dcee628936e3cbd021c7eb59
    Author: JamaJ1701 <48624318+JamaJ1701@users.noreply.github.com>
    Date:   Fri Oct 29 23:08:41 2021 +1000
    
        name conversion added, create new resources functional. resolving references tested.
    
    commit c3c169b1653f6a817b9c9e876f29d07996611f6b
    Author: JamaJ1701 <48624318+JamaJ1701@users.noreply.github.com>
    Date:   Fri Oct 29 20:48:41 2021 +1000
    
        Tne simple history complete
    
    commit 286a1b46d7e25c94a827ac3c00cd84a0a19c9584
    Author: JamaJ1701 <48624318+JamaJ1701@users.noreply.github.com>
    Date:   Fri Oct 29 12:34:56 2021 +1000
    
        dependancies removed
    
    commit e2db67ff5d5cae5ee3c40f88a1b38626207a5b87
    Author: JamaJ1701 <48624318+JamaJ1701@users.noreply.github.com>
    Date:   Fri Oct 29 12:34:08 2021 +1000
    
        data table dependancy added, simple history page added

--- isbar-complex-react/src/app-simple/IsbarSimpleApp.js
+++ isbar-complex-react/src/app-simple/IsbarSimpleApp.js
@@ -1,12 +1,10 @@
 import React from "react";
 // import TextInputField from "./TextInputField";
 import { IsbarClientContext } from "../IsbarFhirClient";
-import { isbarQuestionnaire, newQuestionnaireResponse } from "./QuestionnaireTemplates";
+// import { isbarQuestionnaire, newQuestionnaireResponse } from "./QuestionnaireTemplates";
 import { SimplePDF } from "./SimplePDF";
 import { PDFDownloadLink } from "@react-pdf/renderer";
-// import { ReactDOM } from "react";
 import TextField from '@mui/material/TextField'
-import simpleQuestionnaire from './res/simpleQuestionnaire'
 import { client } from "fhirclient";
 
 
@@ -19,39 +17,45 @@ export class IsbarSimpleApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      // used true if data is loaded
+      
       loaded: false,
-      error: null,
       // questionnaire response object
       content: null,
       // turns true if it's isobar
       isIsobar: false,
       // indicates saved state
-      saved: "edited",
-      // type of current form
-      formState: "ISOBAR"
+      saved: false,
     };
 
     this.updateResponse = this.updateResponse.bind(this)
   }
 
   componentDidMount() {
+    console.log(this.props.create)
+    if(this.props.create){
+      // create new resource and store that 
+      this.createSimpleIsbar().then(console.log).catch(console.error)
+    } else{
+      // note we are not directly modifying the file in parent.
+      // parent will fetch the updated version via database query
+      this.setState({content: this.props.content})
+    }
     this.context.client.user.read().then(console.log)
-    this.setState({content: this.props.content})
+    
   }
 
-  // check if the given resource is QuestionnaireResponse for isbar
-  // still need some check to see if it is actually for isbar
-  checkExistingResponse(resource) {
-    if (
-      resource.source.reference ===
-      "Patient/" + this.context.client.patient.id &&
-      resource.questionnaire === "Questionnaire/" + this.state.questionnaire.id
-    ) {
-      return true;
-    }
-    return false;
-  }
+  // // check if the given resource is QuestionnaireResponse for isbar
+  // // still need some check to see if it is actually for isbar
+  // checkExistingResponse(resource) {
+  //   if (
+  //     resource.source.reference ===
+  //     "Patient/" + this.context.client.patient.id &&
+  //     resource.questionnaire === "Questionnaire/" + this.state.questionnaire.id
+  //   ) {
+  //     return true;
+  //   }
+  //   return false;
+  // }
 
   // function to send update request
   updateResponse() {
@@ -95,6 +99,14 @@ export class IsbarSimpleApp extends React.Component {
     return this.state.content.item[index].hasOwnProperty('answer')
     ? this.state.content.item[index].answer[0].valueString
     : ""
+  }
+
+  createSimpleIsbar(){
+    return this.context.client.create(newQuestionnaireResponse(
+      this.props.questionnaireID,
+      this.context.client.patient.id,
+      this.context.client.user.id
+    )).catch(console.error)
   }
 
   // Load the text fields after the questionnaire and questionnaire responses are loaded.
@@ -188,4 +200,68 @@ export class IsbarSimpleApp extends React.Component {
       )
     }
   }
+}
+
+// function that returns a new response
+// sourceID: ID of practitioner writing this form
+// targetID: ID of practitioner receiving this form
+function newQuestionnaireResponse(questionnaireID, patientID, sourceID) {
+  // questionnaire response resource
+  //const date = new Date()
+  // const lastModified = new Date().toJSON()
+  // console.log(lastModified)
+  
+  var qResponse = {
+    resourceType: "QuestionnaireResponse",
+    text: { name: "isbar handover form" },
+    // maybe later when we sort out the thingy
+    // Reference the questionnaire
+    // date this form was last modified
+    // authored: lastModified,
+    questionnaire: "Questionnaire/" + questionnaireID,
+    status: "in-progress",
+    subject: {
+      reference: "Patient/" + patientID
+    },
+    author: {
+      // refer to current practitioner
+      reference: "Practitioner/" + sourceID,
+    },
+    // extension not used because resolving reference requires extra effort. 
+    // Instead, author section is used.
+    extension: [{
+      url:"http://hl7.org/fhir/StructureDefinition/questionnaireresponse-reviewer",
+      valueReference:{
+        reference: "Practitioner/" + sourceID
+      }
+    }],
+    item: [
+      {
+        linkId: "1",
+        text: "I:Identify",
+      },
+      {
+        linkId: "2",
+        text: "S:Situation",
+      },
+      {
+        linkId: "3",
+        text: "O:Observation",
+      },
+      {
+        linkId: "4",
+        text: "B:Background",
+      },
+      {
+        linkId: "5",
+        text: "A:Assessment",
+      },
+      {
+        linkId: "6",
+        text: "R:Recommendation",
+      },
+    ],
+  };
+
+  return qResponse;
 }
---
+++ isbar-complex-react/src/app-simple/SimpleHeader.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+
+
+export class SimpleHeader extends React.PureComponent {
+
+}
