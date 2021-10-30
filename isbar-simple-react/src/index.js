026f222a8dea71285bb9d5415e787ad62316ce52
JamaJ1701 <48624318+JamaJ1701@users.noreply.github.com>
2021-10-31 09:00:10 +1000

    Files removed

--- isbar-simple-react/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
-// Render the ISBAR application in index.html
-ReactDOM.render(<App />, document.getElementById("root"));
