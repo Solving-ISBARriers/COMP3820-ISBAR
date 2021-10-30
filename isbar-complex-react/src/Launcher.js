e801e6116f56b270bd353f7a028fe7b22f9dcae0
oscardunstan <oscardunstan2@gmail.com>
2021-10-31 00:13:18 +1000

    try iss

--- isbar-complex-react/src/Launcher.js
+++ isbar-complex-react/src/Launcher.js
@@ -16,12 +16,16 @@ export default class Launcher extends React.Component {
      * is loaded directly. An EHR can still launch it by passing `iss`
      * and `launch` url parameters
      */
-    componentDidMount() {
-        
+    componentDidMount() {        
         SMART.authorize({
             clientId: "my-client-id",
             scope: "launch openid fhirUser patient/*.read patient/*.write",
             redirectUri: "./isbar-app",
+            iss:
+                "https://launch.smarthealthit.org/v/r3/sim/" +
+                "eyJoIjoiMSIsImIiOiJmMDQ2MjkzNi1lYjRiLTRkYT" +
+                "EtYjQ1YS1mYmQ5NmViZjhjY2IiLCJlIjoic21hcnQt" +
+                "UHJhY3RpdGlvbmVyLTcxNjE0NTAyIn0/fhir",
             // Redirect to main screen.
             
             completeInTarget: true
